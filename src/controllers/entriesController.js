3c78f57afbba2a23d3fb0fe6b0ac7d7c8305d099
Sandhya <sandhya.dhimal00@gmail.com>
2026-08-30 14:41:37 +0545

    add entry CRUD create read and update

--- src/controllers/entriesController.js
+++ src/controllers/entriesController.js
@@ -33,3 +33,63 @@ export function createEntries(req,res){
     entries.push(newEntry);
     return res.status(200).json(newEntry);
 }
+
+export function getEntryById(req,res){
+    const id = req.params.id;
+
+    const entry = entries.find((entry)=>{
+        return entry.id === Number(id);
+    });
+
+    if(!entry){
+        return res.status(404).json({
+            message : "entry not found"
+        })
+    }
+
+    return res.status(200).json(entry);
+}
+
+export function updateEntry(req,res){
+    const { title, content, mood, tags } = req.body;
+    const id = req.params.id;
+
+    const updateData = {};
+
+    if(title !== undefined){
+        updateData.title = title;
+    }
+
+    if(content !== undefined){
+        updateData.content = content;
+    }
+
+    if(mood !== undefined){
+        updateData.mood = mood;
+    }
+
+    if(tags !== undefined){
+        updateData.tags = tags;
+    }
+    
+    if(Object.keys(updateData).length === 0){
+        return res.status(400).json({
+            message : "No field to updated"
+        })
+    }
+
+    const entry = entries.find((entry)=>{
+        return entry.id === Number(id);
+    });
+
+    if(!entry){
+        return res.status(404).json({
+            message : "Entry doesnot exist"
+        });
+    }
+
+    Object.assign(entry, updateData);
+
+    return res.status(200).json(entry);
+    
+}
--- src/routes/entries.js
+++ src/routes/entries.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { getEntries, 
-    createEntries
+    createEntries,
+    getEntryById,
+    updateEntry
  } from "../controllers/entriesController.js";
 
 const router = express.Router();
 
 router.get("/",getEntries);
 router.post("/",createEntries);
+router.get("/:id", getEntryById);
+router.patch("/:id",updateEntry);
 
 export default router;
