6aba1e4b48d926ba78138f66726a14619ff5e616
Sandhya <sandhya.dhimal00@gmail.com>
2026-09-07 16:57:08 +0545

    fix: validate authorization header

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,17 +3,16 @@ import { AppError } from "./appError.js";
 
 export function authMiddleware(req,res,next){
     const authHeader = req.headers.authorization;
-    
-    if(!authHeader){
-        throw new AppError("no authHeader available",401);
+
+    const [scheme, token] = authHeader.split(" ");
+
+    if(scheme !== "Bearer" || !token){
+        throw new AppError("Invalid authorization header",401);
     }
-
-    const authToken = authHeader.split(" ")[1];
-
     
      try {
         const decoded = jwt.verify(
-            authToken,
+            token,
             process.env.JWT_SECRET
         );
 
